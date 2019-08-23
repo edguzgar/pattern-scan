cc84784dc3bd8d8765f1e07dd9bedb454fa5ab61
edguzgar <42489302+edguzgar@users.noreply.github.com>
2019-08-23 16:24:42 +0200

    Update pattern_scan.c

--- src/pattern_scan.c
+++ src/pattern_scan.c
@@ -1,20 +1,25 @@
 #include "pattern_scan.h"
 
 #include <string.h>
+#include <stddef.h>
 
-void* find_pattern(unsigned char* pattern, char* mask, unsigned char* buffer, size_t n)
+void* FindPattern(const unsigned char* pattern, const char* mask, const unsigned char* buffer, size_t buffer_size)
 {
-    size_t m = strlen(mask);
-    unsigned char* end = buffer + n - m;
+    size_t mask_size = strlen(mask);
+
+    if (mask_size == 0)
+        return NULL;
+
+    const unsigned char* end = buffer + buffer_size - mask_size;
 
     for (; buffer < end; ++buffer) {
         size_t i;
 
-        for (i = 0; i < m; ++i)
+        for (i = 0; i != mask_size; ++i) {
             if (mask[i] != '?' && buffer[i] != pattern[i])
                 break;
-
-        if (i == m)
+        }
+        if (i == mask_size)
             return buffer + (strchr(mask, '?') - mask);
     }
 
