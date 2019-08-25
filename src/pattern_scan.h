2d109dd97969f790126535b22f282b9adeddc66b
edguzgar <42489302+edguzgar@users.noreply.github.com>
2019-08-25 12:14:05 +0200

    Update pattern_scan.h

--- src/pattern_scan.h
+++ src/pattern_scan.h
@@ -4,10 +4,11 @@
 #include <stddef.h>
 
 /*
-  Simple function that search a value in an array of bytes that is located
-  between the pattern bounds and return its pointer.
-  It uses a simple and naive algorithm, for better performance use KMP.
-  
+
+   Simple function that search a value in an array of bytes that is located
+   between the pattern bounds and return its pointer.
+   It uses a simple and naive algorithm, for better performance use KMP.
+   
  */
 
 void* find_pattern(void* pattern, char* mask, void* buffer, size_t buffer_size);
