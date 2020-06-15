a1faa6941ecb3c529df9c540ddbcd4ad2f07bbd2
edguzgar <42489302+edguzgar@users.noreply.github.com>
2020-06-15 02:58:43 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -49,7 +49,7 @@ int main(void)
 
     unsigned char health_pattern[] = "\x89\x41\x64\x8B\x4F\x04\x83\xB9\x00\x00\x00\x00\x00\x7F\x2E\x8B\x01\x8B\x80\x88\x04\x00\x00";
     char health_mask[] = "xxxxxxxx????xxxxxxxxxxx";
-
+    /*
     ptrdiff_t health_ptr = find_pattern(buffer, module.modBaseSize, health_pattern, health_mask);
 
     if (health_ptr == NULL) 
@@ -61,6 +61,6 @@ int main(void)
     printf("The value of the Health Offset is: %#x", health_ptr);
     
     free(buffer);
-
+    */
     return EXIT_SUCCESS;
 }
