c480a9d5bf3bf2f5944519d272bc50156bf996de
eduguzgar <42489302+eduguzgar@users.noreply.github.com>
2021-07-07 17:06:18 +0200

    Update string.h

--- src/string.h
+++ src/string.h
@@ -2,8 +2,8 @@
 #define STRING_H
 
 char*           scan(char* string);                 // Read dynamically allocated string of stdin
-int             count(char str[], char search);     // Count ocurrences of search character (search) in the string (str)
-void            remove_char(char* s, int c);        // Remove char
+int             count(char* str, char search);      // Count ocurrences of search character "search" in the string "str"
+void            remove_char(char* str, int c);      // Remove specified char "c" in the string "str"
 unsigned char*  hexstr_to_char(const char* hexstr); // Turn a hex string to unsigned char (Autor: xsleonard, https://gist.github.com/xsleonard/7341172)
 
 #endif // !STRING_H
